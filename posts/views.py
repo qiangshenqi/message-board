fdf7ef0a299169619a1c5046fb7ac4449adf17e7
zhaoqiang <zhaoqiang@bsghd.com>
2025-12-27 17:40:27 +0800

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,11 @@
+from symtable import Class
+
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+# Create your views here.
+
+class HomepageView(ListView):
+    model = Post
+    template_name = 'home.html'
